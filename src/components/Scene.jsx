a54262b2835a0ae7220f09fd2907ad3e2d25a02b
Olivier Fortier <olivier_fortier@outlook.com>
2021-03-14 22:13:27 -0400

    texte ar

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,112 +1,129 @@
-import React from "react";
-import { Canvas } from "react-three-fiber";
+import React, {useEffect, useState} from "react";
 import { OrbitControls, Billboard, Html, Text } from "@react-three/drei";
 import { ARCanvas } from "@react-three/xr";
 
 export default function Scene() {
+
+  const [hover,setHover] = useState(false);
+
+  useEffect(() => {
+    document.body.style.cursor = hover ? 'pointer' : 'auto'
+  }, [hover])
+  
   return (
-    <ARCanvas style={{ height: "100vh" }}>
-      <OrbitControls />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Billboard
-        position={[-4, -2, -10]}
-        args={[3, 2]}
-        material-color="#003261"
-        follow={true}
-        lockX={false}
-        lockY={false}
-        lockZ={false}
-      >
-        <Text
-          color={"#e26060"}
-          fontSize={0.3}
-          maxWidth={5}
-          lineHeight={1}
-          letterSpacing={0.02}
-          textAlign={"left"}
-          font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
-          anchorX="center"
-          anchorY="middle"
+    <>
+      <ARCanvas style={{ height: "100vh" }}>
+        <OrbitControls />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+
+        <Billboard
+          position={[-4, -2, -4]}
+          args={[3, 2]}
+          material-color="#003261"
+          follow={true}
+          lockX={false}
+          lockY={false}
+          lockZ={false}
         >
-          NOUS SOMMES EN RÉALITÉ AUGMENTÉE AVEC JAVASCRIPT. SUR LE WEB.
-        </Text>
-      </Billboard>
-      <Billboard
-        position={[-4, 2, -10]}
-        args={[3, 2]}
-        material-color="orange"
-        follow={true}
-        lockX={false}
-        lockY={false}
-        lockZ={false}
-      >
-        <Html prepend center fullscreen distanceFactor={10} transform>
-          <p>
-            prototype d'interface <br /> nouveau genre
-          </p>
-        </Html>
-      </Billboard>
-      <Billboard
-        position={[0, 0, -10]}
-        args={[3, 2]}
-        material-color="green"
-        follow={true}
-        lockX={false}
-        lockY={false}
-        lockZ={false}
-      >
-        <Text
-          color={"#e26060"}
-          fontSize={0.3}
-          maxWidth={2}
-          lineHeight={1}
-          letterSpacing={0.02}
-          textAlign={"left"}
-          font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
-          anchorX="center"
-          anchorY="middle"
-        >
-          BIENVENUE DANS LE FUTUR DU WEB
-        </Text>
-      </Billboard>
-      <Billboard
-        position={[4, -2, -10]}
-        args={[3, 2]}
-        material-color="blue"
-        follow={true}
-        lockX={false}
-        lockY={false}
-        lockZ={false}
-      >
-        <Html prepend center fullscreen distanceFactor={10} transform>
-          <p className="text-white">
-            aucune <br /> contrainte.
-          </p>
-          <p className="text-white">
-            on peut mixer du HTML à l'intérieur des objets
-          </p>
-        </Html>
-      </Billboard>
-      <Billboard
-        position={[4, 2, -10]}
-        args={[3, 2]}
-        material-color="yellow"
-        follow={true}
-        lockX={false}
-        lockY={false}
-        lockZ={false}
-      >
-        <Html prepend center fullscreen distanceFactor={10} transform>
-          <h5>ceci est un bouton</h5>
-          <button
-            onClick={() => window.appHistory.push("/physique")}
-            className="btn"
+          <Text
+            color={"#e26060"}
+            fontSize={0.3}
+            maxWidth={5}
+            lineHeight={1}
+            letterSpacing={0.02}
+            textAlign={"left"}
+            font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+            anchorX="center"
+            anchorY="middle"
           >
-            cliquez-moi
-          </button>
-        </Html>
-      </Billboard>
-    </ARCanvas>
+            NOUS SOMMES EN RÉALITÉ AUGMENTÉE AVEC JAVASCRIPT. SUR LE WEB.
+          </Text>
+        </Billboard>
+
+        <Billboard
+          position={[-4, 2, -4]}
+          args={[3, 2]}
+          material-color="orange"
+          follow={true}
+          lockX={false}
+          lockY={false}
+          lockZ={false}
+        >
+          <Html prepend center fullscreen distanceFactor={10} transform>
+            <p>
+              prototype d'interface <br /> nouveau genre
+            </p>
+          </Html>
+        </Billboard>
+        <Billboard
+          position={[0, 0, -4]}
+          args={[3, 2]}
+          material-color="green"
+          follow={true}
+          lockX={false}
+          lockY={false}
+          lockZ={false}
+        >
+          <Text
+            color={"#e26060"}
+            fontSize={0.3}
+            maxWidth={2}
+            lineHeight={1}
+            letterSpacing={0.02}
+            textAlign={"left"}
+            font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+            anchorX="center"
+            anchorY="middle"
+          >
+            BIENVENUE DANS LE FUTUR DU WEB
+          </Text>
+        </Billboard>
+        <Billboard
+          position={[4, -2, -4]}
+          args={[3, 2]}
+          material-color="blue"
+          follow={true}
+          lockX={false}
+          lockY={false}
+          lockZ={false}
+        >
+          <Html prepend center fullscreen distanceFactor={10} transform>
+            <p className="text-white">
+              aucune <br /> contrainte.
+            </p>
+            <p className="text-white">
+              on peut mixer du HTML à l'intérieur des objets
+            </p>
+          </Html>
+        </Billboard>
+        <Billboard
+          position={[4, 2, -4]}
+          args={hover ? [4,3] : [3, 2]}
+          material-color="yellow"
+          follow={true}
+          lockX={false}
+          lockY={false}
+          lockZ={false}
+          onPointerUp={() => window.appHistory.push("/physique")}
+          onPointerOver={()=>setHover(true)}
+          onPointerOut={()=>setHover(false)}
+        >
+           <Text
+            color={"#000000"}
+            fontSize={0.3}
+            maxWidth={2}
+            lineHeight={1}
+            letterSpacing={0.02}
+            textAlign={"left"}
+            font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+            anchorX="center"
+            anchorY="middle"
+          >
+           Cliquez-moi !!
+          </Text>
+        </Billboard>
+      </ARCanvas>
+    </>
   );
 }
