4c0d31e6182e6ff165d4a95bc60a703780e38f76
Olivier Fortier <olivier_fortier@outlook.com>
2021-03-14 21:48:50 -0400

    ajout AR

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import { Canvas } from "react-three-fiber";
-import { OrbitControls, Billboard, Html } from "@react-three/drei";
+import { OrbitControls, Billboard, Html, Text } from "@react-three/drei";
+import { ARCanvas } from "@react-three/xr";
 
 export default function Scene() {
   return (
-    <Canvas style={{ height: "100vh" }}>
+    <ARCanvas style={{ height: "100vh" }}>
       <OrbitControls />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Billboard
-        position={[-4, -2, 0]}
+        position={[-4, -2, -10]}
         args={[3, 2]}
-        material-color="red"
+        material-color="#003261"
         follow={true}
         lockX={false}
         lockY={false}
         lockZ={false}
       >
-        <Html prepend center fullscreen distanceFactor={10} transform>
-          <p>
-            Imaginez <br /> les possibilités
-          </p>
-        </Html>
+        <Text
+          color={"#e26060"}
+          fontSize={0.3}
+          maxWidth={5}
+          lineHeight={1}
+          letterSpacing={0.02}
+          textAlign={"left"}
+          font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+          anchorX="center"
+          anchorY="middle"
+        >
+          NOUS SOMMES EN RÉALITÉ AUGMENTÉE AVEC JAVASCRIPT. SUR LE WEB.
+        </Text>
       </Billboard>
       <Billboard
-        position={[-4, 2, 0]}
+        position={[-4, 2, -10]}
         args={[3, 2]}
         material-color="orange"
         follow={true}
@@ -39,7 +48,7 @@ export default function Scene() {
         </Html>
       </Billboard>
       <Billboard
-        position={[0, 0, 0]}
+        position={[0, 0, -10]}
         args={[3, 2]}
         material-color="green"
         follow={true}
@@ -47,13 +56,22 @@ export default function Scene() {
         lockY={false}
         lockZ={false}
       >
-        <Html prepend center fullscreen distanceFactor={10} transform>
-          <h1>hello</h1>
-          <p>world</p>
-        </Html>
+        <Text
+          color={"#e26060"}
+          fontSize={0.3}
+          maxWidth={2}
+          lineHeight={1}
+          letterSpacing={0.02}
+          textAlign={"left"}
+          font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+          anchorX="center"
+          anchorY="middle"
+        >
+          BIENVENUE DANS LE FUTUR DU WEB
+        </Text>
       </Billboard>
       <Billboard
-        position={[4, -2, 0]}
+        position={[4, -2, -10]}
         args={[3, 2]}
         material-color="blue"
         follow={true}
@@ -63,12 +81,15 @@ export default function Scene() {
       >
         <Html prepend center fullscreen distanceFactor={10} transform>
           <p className="text-white">
-            aucune <br /> contrainte
+            aucune <br /> contrainte.
+          </p>
+          <p className="text-white">
+            on peut mixer du HTML à l'intérieur des objets
           </p>
         </Html>
       </Billboard>
       <Billboard
-        position={[4, 2, 0]}
+        position={[4, 2, -10]}
         args={[3, 2]}
         material-color="yellow"
         follow={true}
@@ -78,9 +99,14 @@ export default function Scene() {
       >
         <Html prepend center fullscreen distanceFactor={10} transform>
           <h5>ceci est un bouton</h5>
-          <button onClick={() => window.appHistory.push("/physique")} className="btn">cliquez-moi</button>
+          <button
+            onClick={() => window.appHistory.push("/physique")}
+            className="btn"
+          >
+            cliquez-moi
+          </button>
         </Html>
       </Billboard>
-    </Canvas>
+    </ARCanvas>
   );
 }
